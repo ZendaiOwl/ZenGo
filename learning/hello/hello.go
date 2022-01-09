911b5a71c06ba66620e8ff7e9c84e75cdc95c22e
Victor-ray ( ZendaiOwl ) <victorray91@pm.me>
2022-01-09 01:23:14 +0100

    Refactored the use English, Spanish and French while passing the test

--- learning/hello/hello.go
+++ learning/hello/hello.go
@@ -2,19 +2,32 @@ package main
 
 import "fmt"
 
+const english = "English"
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 func Hello(name string, language string) string {
 	if name == "" {
-		name = "World"
+	name = "World"
 	}
+	return greetingPrefix(language) + name
+}
 
-	if language == "Spanish" {
-		return "Hola, " + name
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
 	}
-	return englishHelloPrefix + name
+	return
 }
 
 func main() {
-	fmt.Println(Hello("", ""))
+	fmt.Println(Hello("Magic", "English"))
 }
