5b9ddcc08428705979802c05377909874b5982fe
Victor-ray ( ZendaiOwl ) <victorray91@pm.me>
2022-01-14 20:59:01 +0100

    Changed the function and test so it passes with an array slice

--- learning/arrays/sum.go
+++ learning/arrays/sum.go
@@ -1,9 +1,17 @@
 package main
 
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return sum
 }
+
+// func Sum(numbers [5]int) int {
+	// sum := 0
+	// for _, number := range numbers {
+		// sum += number
+	// }
+	// return sum
+// }
