694e78735b8b43497a106e9b5c42cbbc6e50718e
Victor-ray ( ZendaiOwl ) <victorray91@pm.me>
2022-01-14 22:29:09 +0100

    Refactor

--- learning/arrays/sum.go
+++ learning/arrays/sum.go
@@ -1,11 +1,9 @@
 package main
 
-func SumAll(numbersToSum ...[]int) (sums []int) {
-	lengthOfNumbers := len(numbersToSum)
-	sums = make([]int, lengthOfNumbers)
-
-	for i,numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
 	}
 	return sums
 }
