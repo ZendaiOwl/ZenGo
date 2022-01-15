695ae1398f474eca138dc399840073ad8c023274
Victor-ray ( ZendaiOwl ) <victorray91@pm.me>
2022-01-15 19:17:24 +0100

    Added circle

--- learning/constructs/shapes.go
+++ learning/constructs/shapes.go
@@ -1,5 +1,24 @@
 package main
 
+import "math"
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
+}
+
+type Rectangle struct {
+	Width float64
+	Height float64
+}
+
+type Circle struct {
+	Radius float64
+}
+
 func Area(rectangle Rectangle) float64 {
 	return (rectangle.Width * rectangle.Height)
 }
--- learning/constructs/shapes_test.go
+++ learning/constructs/shapes_test.go
@@ -2,18 +2,24 @@ package main
 
 import "testing"
 
-type Rectangle struct {
-	Width float64
-	Height float64
-}
-
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{6.0,12.0}
-	got := Area(rectangle)
-	want := 72.0
-	if got != want {
-		t.Errorf("got %.2f want %.2f",got,want)
-	}
+	t.Run("rectangles",func(t *testing.T){
+		rectangle := Rectangle{6,12}
+		got := rectangle.Area()
+		want := 72.0
+		if got != want {
+			t.Errorf("got %.2f want %.2f",got,want)
+		}
+	})
+
+	t.Run("circles",func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
 
 func TestPerimiter(t *testing.T) {
