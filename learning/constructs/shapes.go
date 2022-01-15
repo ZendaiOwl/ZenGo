99dc820223ace6b943014f70fb19cfa32317dd5d
Victor-ray ( ZendaiOwl ) <victorray91@pm.me>
2022-01-15 18:49:43 +0100

    Added Area

--- learning/constructs/shapes.go
+++ learning/constructs/shapes.go
@@ -1,5 +1,9 @@
 package main
 
+func Area(width float64, height float64) float64 {
+	return (width * height)
+}
+
 func Perimiter(width float64, height float64) float64 {
 	return 2 * (width + height)
 }
--- learning/constructs/shapes_test.go
+++ learning/constructs/shapes_test.go
@@ -2,6 +2,14 @@ package main
 
 import "testing"
 
+func TestArea(t *testing.T) {
+	got := Area(6.0,12.0)
+	want := 72.0
+	if got != want {
+		t.Errorf("got %.2f want %.2f",got,want)
+	}
+}
+
 func TestPerimiter(t *testing.T) {
 	got := Perimiter(10.0, 10.0)
 	want := 40.0
